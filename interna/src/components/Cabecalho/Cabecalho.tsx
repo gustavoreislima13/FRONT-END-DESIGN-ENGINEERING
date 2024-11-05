0571fe127bf1218f937feebf19563ef9366468d9
gustavoreislima13 <reisanselmo7@gmail.com>
2024-11-04 21:34:38 -0300

    mundao componentes

---
+++ interna/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/image/logo.png';
+
+
+export default function Cabecalho(): JSX.Element {
+  return (
+    <header className="flex justify-between items-center p-4 bg-black w-full">
+      <div className="flex items-center">
+        <Image src={logo} alt="" width={40} height={40} />
+      </div>
+      
+      <nav className="flex space-x-4">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/" className="bg-transparent text-white px-4 py-2 rounded-full hover:bg-gray-700 transition">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/servico" className="bg-transparent text-white px-4 py-2 rounded-full hover:bg-gray-700 transition">
+            Serviços
+            </Link>
+          </li>
+          
+          <li>
+            <Link href="/dashboard" className="bg-transparent text-white px-4 py-2 rounded-full hover:bg-gray-700 transition">
+            Dashboard
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
